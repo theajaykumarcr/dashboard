12e65f4d6c4bc6eb22c4b12fd6a6d12e2403ff25
Ajay kumar C R <ajaycrclk@outlook.com>
2023-03-02 23:52:07 +0530

    dashboard new 2march

---
+++ SideBar.js
@@ -0,0 +1,401 @@
+// /* eslint-disable no-unused-vars */
+// /* eslint-disable react/jsx-no-undef */
+// /* eslint-disable no-undef */
+// import * as React from "react";
+// import { styled, useTheme } from "@mui/material/styles";
+// import Box from "@mui/material/Box";
+// import MuiDrawer from "@mui/material/Drawer";
+// import { useNavigate } from "react-router-dom";
+// import List from "@mui/material/List";
+// import CssBaseline from "@mui/material/CssBaseline";
+// import Typography from "@mui/material/Typography";
+// import Divider from "@mui/material/Divider";
+// import IconButton from "@mui/material/IconButton";
+// import MenuIcon from "@mui/icons-material/Menu";
+// import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+// import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+// import ListItem from "@mui/material/ListItem";
+// import ListItemButton from "@mui/material/ListItemButton";
+// import ListItemIcon from "@mui/material/ListItemIcon";
+// import InboxIcon from "@mui/icons-material/MoveToInbox";
+// import ListItemText from "@mui/material/ListItemText";
+// import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+// import Collapse from "@mui/material/Collapse";
+// import { useAppStore } from "../appStore";
+
+// const drawerWidth = 240;
+
+// const openedMixin = (theme) => ({
+//   width: drawerWidth,
+//   transition: theme.transitions.create("width", {
+//     easing: theme.transitions.easing.sharp,
+//     duration: theme.transitions.duration.enteringScreen,
+//   }),
+//   overflowX: "hidden",
+// });
+
+// const closedMixin = (theme) => ({
+//   transition: theme.transitions.create("width", {
+//     easing: theme.transitions.easing.sharp,
+//     duration: theme.transitions.duration.leavingScreen,
+//   }),
+//   overflowX: "hidden",
+//   width: `calc(${theme.spacing(7)} + 1px)`,
+//   [theme.breakpoints.up("sm")]: {
+//     width: `calc(${theme.spacing(8)} + 1px)`,
+//   },
+// });
+
+// const DrawerHeader = styled("div")(({ theme }) => ({
+//   display: "flex",
+//   alignItems: "center",
+//   justifyContent: "flex-end",
+//   padding: theme.spacing(0, 1),
+//   // necessary for content to be below app bar
+//   ...theme.mixins.toolbar,
+// }));
+
+// const Drawer = styled(MuiDrawer, {
+//   shouldForwardProp: (prop) => prop !== "open",
+// })(({ theme, open }) => ({
+//   width: drawerWidth,
+//   flexShrink: 0,
+//   whiteSpace: "nowrap",
+//   boxSizing: "border-box",
+//   ...(open && {
+//     ...openedMixin(theme),
+//     "& .MuiDrawer-paper": openedMixin(theme),
+//   }),
+//   ...(!open && {
+//     ...closedMixin(theme),
+//     "& .MuiDrawer-paper": closedMixin(theme),
+//   }),
+// }));
+
+// export default function SideBar() {
+//   const theme = useTheme();
+//   const Navigate = useNavigate();
+//   const updateOpen = useAppStore((state) => state.updateOpen);
+//   const open = useAppStore((state) => state.dopen);
+
+//   const [openSubmenu, setOpenSubmenu] = React.useState(false);
+
+//   const handleSubmenuClick = () => {
+//     setOpenSubmenu(!openSubmenu);
+//   };
+
+//   return (
+//     <Box sx={{ display: "flex" }} elevation={2}>
+//       <CssBaseline />
+//       <Box height={30} />
+//       <Drawer variant="permanent" open={open}>
+//         <DrawerHeader>
+//           <IconButton>
+//             {theme.direction === "rtl" ? (
+//               <ChevronRightIcon />
+//             ) : (
+//               <ChevronLeftIcon />
+//             )}
+//           </IconButton>
+//         </DrawerHeader>
+//         <Divider />
+//         <List>
+//           <ListItem
+//             disablePadding
+//             sx={{ display: "block" }}
+//             onClick={() => {
+//               Navigate("/");
+//             }}
+//           >
+//             <ListItemButton
+//               sx={{
+//                 minHeight: 48,
+//                 justifyContent: open ? "initial" : "center",
+//                 px: 2.5,
+//               }}
+//             >
+//               <ListItemIcon
+//                 sx={{
+//                   minWidth: 0,
+//                   mr: open ? 3 : "auto",
+//                   justifyContent: "center",
+//                 }}
+//               >
+//                 <InboxIcon />
+//               </ListItemIcon>
+//               <ListItemText primary="Home" sx={{ opacity: open ? 1 : 0 }} />
+//             </ListItemButton>
+//           </ListItem>
+//           <ListItem
+//             disablePadding
+//             sx={{ display: "block" }}
+//             // eslint-disable-next-line no-undef
+//             onClick={handleSubMenuClick}
+//           >
+//             <ListItemButton
+//               sx={{
+//                 minHeight: 48,
+//                 justifyContent: open ? "initial" : "center",
+//                 px: 2.5,
+//               }}
+//             >
+//               <ListItemIcon
+//                 sx={{
+//                   minWidth: 0,
+//                   mr: open ? 3 : "auto",
+//                   justifyContent: "center",
+//                 }}
+//               >
+//                 <InboxIcon />
+//               </ListItemIcon>
+//               <ListItemText
+//                 primary="Submenu"
+//                 sx={{ opacity: open ? 1 : 0 }}
+//               />
+//               {subMenuOpen ? <ExpandLess /> : <ExpandMore />}
+//             </ListItemButton>
+//           </ListItem>
+//           <Collapse in={subMenuOpen} timeout="auto" unmountOnExit>
+//             <List component="div" disablePadding>
+//               <ListItem
+//                 disablePadding
+//                 sx={{ display: "block" }}
+//                 onClick={() => {
+//                   Navigate("/submenu1");
+//                 }}
+//               >
+//                 <ListItemButton
+//                   sx={{
+//                     minHeight: 48,
+//                     justifyContent: open ? "initial" : "center",
+//                     px: 2.5,
+//                   }}
+//                 >
+//                   <ListItemIcon
+//                     sx={{
+//                       minWidth: 0,
+//                       mr: open ? 3 : "auto",
+//                       justifyContent: "center",
+//                     }}
+//                   >
+//                     <InboxIcon />
+//                   </ListItemIcon>
+//                   <ListItemText
+//                     primary="Submenu 1"
+//                     sx={{ opacity: open ? 1 : 0 }}
+//                   />
+//                 </ListItemButton>
+//               </ListItem>
+//               <ListItem
+//                 disablePadding
+//                 sx={{ display: "block" }}
+//                 onClick={() => {
+//                   Navigate("/submenu2");
+//                 }}
+//               >
+//               <ListItemButton
+//               sx={{
+//                 minHeight: 48,
+//                 justifyContent: open ? "initial" : "center",
+//                 px: 2.5,
+//               }}
+//             >
+//               <ListItemIcon
+//                 sx={{
+//                   minWidth: 0,
+//                   mr: open ? 3 : "auto",
+//                   justifyContent: "center",
+//                 }}
+//               >
+//                 <InboxIcon />
+//               </ListItemIcon>
+//               <ListItemText
+//                 primary="Submenu 1"
+//                 sx={{ opacity: open ? 1 : 0 }}
+//               />
+//             </ListItemButton>
+//             </ListItem>
+//             </List>
+//             </Collapse>
+//             </Drawer>
+//             </List>
+
+//             </Box>
+//   )
+//               }
+
+import * as React from "react";
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MuiDrawer from "@mui/material/Drawer";
+import { useNavigate } from "react-router-dom";
+import List from "@mui/material/List";
+import CssBaseline from "@mui/material/CssBaseline";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import ListItemText from "@mui/material/ListItemText";
+import { useAppStore } from "../appStore";
+
+const drawerWidth = 240;
+
+const openedMixin = (theme) => ({
+  width: drawerWidth,
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: "hidden",
+});
+
+const closedMixin = (theme) => ({
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: "hidden",
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
+}));
+
+export default function SideBar() {
+  const theme = useTheme();
+  //   const [open, setOpen] = React.useState(true);
+  const Navigate = useNavigate();
+  const updateOpen = useAppStore((state) => state.updateOpen);
+  const open = useAppStore((state) => state.dopen);
+
+  return (
+    <Box sx={{ display: "flex" }} elevation={2}>
+      <CssBaseline />
+      <Box height={30} />
+      <Drawer variant="permanent" open={open}>
+        <DrawerHeader>
+          <IconButton>
+            {theme.direction === "rtl" ? (
+              <ChevronRightIcon />
+            ) : (
+              <ChevronLeftIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          <ListItem
+            disablePadding
+            sx={{ display: "block" }}
+            onClick={() => {
+              Navigate("/");
+            }}
+          >
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? "initial" : "center",
+                px: 2.5,
+              }}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : "auto",
+                  justifyContent: "center",
+                }}
+              >
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary="Home" sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+          </ListItem>
+          <ListItem
+            disablePadding
+            sx={{ display: "block" }}
+            onClick={() => {
+              Navigate("/about");
+            }}
+          >
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? "initial" : "center",
+                px: 2.5,
+              }}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : "auto",
+                  justifyContent: "center",
+                }}
+              >
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary="About" sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+          </ListItem>
+          <ListItem
+            disablePadding
+            sx={{ display: "block" }}
+            onClick={() => {
+              Navigate("/setting");
+            }}
+          >
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? "initial" : "center",
+                px: 2.5,
+              }}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : "auto",
+                  justifyContent: "center",
+                }}
+              >
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary="Setting " sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Drawer>
+    </Box>
+  );
+}
